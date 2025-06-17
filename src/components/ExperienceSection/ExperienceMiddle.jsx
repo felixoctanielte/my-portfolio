2e3f56103ca4bd9c18e3b1f5e59ee9625afd1b89
Felix Octaniel Telaumbanua <felixoctanieltel05@gmail.com>
2025-06-17 21:53:37 +0700

    Update ExperienceMiddle.jsx

--- src/components/ExperienceSection/ExperienceMiddle.jsx
+++ src/components/ExperienceSection/ExperienceMiddle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ExperienceMiddle = () => {
   return (
     <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
-        <img src="../../public/images/experience-image.png" 
+        <img src="/images/experience-image.png" 
         alt="Experience Image" />
     </div>
   );
