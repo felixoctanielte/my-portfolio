ced59eda90bcb07da003c17c0155a5382d6f4b8d
Felix Octaniel Telaumbanua <felixoctanieltel05@gmail.com>
2025-06-17 21:50:36 +0700

    Update AboutMeImages.jsx

--- src/components/aboutMeSection/AboutMeImages.jsx
+++ src/components/aboutMeSection/AboutMeImages.jsx
@@ -4,7 +4,7 @@ const AboutMeImages = () => {
   return (
     <div className="h-[500px] w-[300px] relative">
         <div className="h-[500px] w-[300px] rounded-[100px] absolute  overflow-hidden">
-            <img src="../../public/images/about-me.jpg" 
+            <img src="/images/about-me.jpg" 
             alt="about-me image" 
             className="h-full w-auto object-cover"/>
         </div>
