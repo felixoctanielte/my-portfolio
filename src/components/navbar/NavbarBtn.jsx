37f7e98702e9354400e1050592d7a7ec0eabe10d
Felix Octaniel Telaumbanua <felixoctanieltel05@gmail.com>
2025-06-17 22:00:14 +0700

    Update NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -4,7 +4,7 @@ import { LuArrowDownRight } from "react-icons/lu";
 const NavbarBtn = () => {
   return (
     <a
-      href="/public/folder pdf/CV_Felix Octaniel T.pdf" 
+      href="/folder pdf/CV_Felix Octaniel T.pdf" 
       download
       className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow"
     >
